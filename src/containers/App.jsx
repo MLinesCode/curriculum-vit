c19e3088a13ab15bccd7e6f9fc2cf30323251147
Specter <mikexx1264@gmail.com>
2020-07-04 20:14:21 -0500

    Custom hook created

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -7,8 +7,11 @@ import Education from '../components/Education';
 import Experience from '../components/Experience';
 import Certificates from '../components/Certificates';
 import Skills from '../components/Skills';
+import useGetData from '../hooks/useGetData';
 
 const App = () => {
+  const data = useGetData();
+  console.log(data)
   return (
     <Main>
       <Sidebar>
